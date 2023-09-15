daba0c25370c7513efb107abe19a10f60575ba68
Gavaskar Muppavaram <117184742+Gavaskar24@users.noreply.github.com>
2023-09-15 12:01:13 +0530

    Add files via upload

---
+++ passenger_arrival.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from scipy.stats import norm
+from scipy.stats import expon
+import matplotlib.pyplot as plt
+
+# Passenger arrival times are to be modelled from inter-arrival time data from file.csv
+df=pd.read_csv('file.csv')
+
+# First column Ben is interarrival times of passengers towards A
+# Second column Ben is interarrival times of passengers towards B
+
+# Plotting histogram of inter-arrival times towards A
+plt.hist(df['Ben'], bins=20)
+plt.title("Inter-arrival times towards A")
+plt.xlabel("Inter-arrival times")
+plt.ylabel("Frequency")
+plt.show()
+
+#It looks like exponential distribution, lets try to fit exponential distribution to this data
+
+sns.set(color_codes=True)
+sns.set(rc={'figure.figsize':(5,5)})
+x = np.linspace(0, 50, 100)
+data=df['Ben']
+ax = sns.distplot(data,
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1}) 
+ax.set(xlabel='Exponential Distribution ', ylabel='Frequency')
+params = expon.fit(data)
+print("Parameters of exponential distribution",params)
+plt.show()
+plt.figure()
+
+# Plotting histogram of inter-arrival times towards B
+plt.hist(df['Tum'], bins=20)
+plt.title("Inter-arrival times towards B")
+plt.xlabel("Inter-arrival times")
+plt.ylabel("Frequency")
+plt.show()
+
+# As the data seem to follow Normal distribution fitting Normal distribution to the inter-arrival times towards B
+
+sns.set(color_codes=True)
+sns.set(rc={'figure.figsize':(5,5)})
+x = np.linspace(0, 50, 100)
+data=df['Tum']
+ax = sns.distplot(data,
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Normal Distribution ', ylabel='Frequency')
+params = norm.fit(data)
+print("Parameters of Normal distribution",params)
+
+plt.show()
+
---
+++ train_station.py
@@ -0,0 +1,92 @@
+import simpy
+import random
+
+
+# Parameters
+num_passengers = 100000  # Total number of passengers
+ticket_issue_rate_clerk = 1  # Exponential distribution rate for ticket issue at clerk
+ticket_issue_rate_machine = 0.7  # Exponential distribution rate for ticket issue at machine
+machine_failure_mean = 1000  # Mean of the normal distribution for machine failure rate
+machine_failure_std = 200  # Standard deviation of the normal distribution for machine failure rate
+repair_time_rate = 1 / 30  # Rate parameter for repair time exponential distribution
+train_A_interarrival_rate = 1 / 14  # Rate parameter for train A inter-arrival time exponential distribution
+train_B_interarrival_rate = 1 / 25  # Rate parameter for train B inter-arrival time exponential distribution
+seats_per_train = 120  # Total number of seats per train
+seat_min = 15  # Minimum seat availability Uniform Random variable
+seat_max = 35  # Maximum seat availability Uniform Random variable
+
+# Functions for random events
+def generate_interarrival_time(rate):
+    return random.expovariate(rate)
+
+def generate_ticket_issue_time(is_machine):
+    if is_machine:
+        return random.expovariate(ticket_issue_rate_machine)
+    else:
+        return random.expovariate(ticket_issue_rate_clerk)
+
+def generate_machine_failure_rate():
+    return max(0, random.normalvariate(machine_failure_mean, machine_failure_std))
+
+# Simulation
+class TrainStation(object):
+    def __init__(self, env):
+        self.env = env
+        self.machine = simpy.Resource(env, capacity=1)
+        self.train_A = simpy.Resource(env, capacity=seats_per_train)
+        self.train_B = simpy.Resource(env, capacity=seats_per_train)
+        self.total_passengers = 0
+        self.waiting_times = []
+        self.ticket_purchase_times = []
+
+    def purchase_ticket(self, passenger):
+        yield self.env.timeout(generate_ticket_issue_time(passenger.is_machine))
+        self.ticket_purchase_times.append(self.env.now)
+        if passenger.is_machine:
+            failure_rate = generate_machine_failure_rate()
+            yield self.env.timeout(failure_rate)  # Machine failure time
+            repair_time = random.expovariate(repair_time_rate)
+            yield self.env.timeout(repair_time)  # Machine repair time
+
+    def board_train(self, passenger):
+        if passenger.destination == 'A':
+            yield self.train_A.request()
+        elif passenger.destination == 'B':
+            yield self.train_B.request()
+        
+        seat_availability = random.randint(seat_min, seat_max)
+        yield self.env.timeout(seat_availability)
+        self.total_passengers += 1
+        self.waiting_times.append(self.env.now)
+
+class Passenger(object):
+    def __init__(self, env, name):
+        self.env = env
+        self.name = name
+        self.is_machine = random.choice([True, False])
+        self.destination = random.choice(['A', 'B'])
+        self.action = env.process(self.run())
+
+    def run(self):
+        arrival_time = self.env.now
+        yield self.env.process(station.purchase_ticket(self))
+        yield self.env.process(station.board_train(self))
+        print(f"Passenger {self.name} boarded train {self.destination} at time {self.env.now - arrival_time:.2f}")
+
+# Initialize the simulation environment
+env = simpy.Environment()
+station = TrainStation(env)
+
+# Create passengers
+for i in range(num_passengers):
+    passenger = Passenger(env, i)
+
+# Run the simulation
+env.run()
+
+# Calculate and print results
+average_ticket_purchase_time = sum(station.ticket_purchase_times) / len(station.ticket_purchase_times)
+average_waiting_time = sum(station.waiting_times) / station.total_passengers
+
+print(f"Average Ticket Purchase Time for 90% of Passengers: {average_ticket_purchase_time:.2f}")
+print(f"Average Waiting Time for All Passengers: {average_waiting_time:.2f}")
