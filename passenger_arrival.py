ca9fbc28127bb3ca9d46cbd501599f911eda10e1
Gavaskar <mgavaskar24@gmail.com>
2023-09-22 12:17:26 +0530

    Update passenger_arrival.py

--- passenger_arrival.py
+++ passenger_arrival.py
@@ -20,42 +20,62 @@ plt.show()
 
 #It looks like exponential distribution, lets try to fit exponential distribution to this data
 
-sns.set(color_codes=True)
-sns.set(rc={'figure.figsize':(5,5)})
-x = np.linspace(0, 50, 100)
-data=df['Ben']
-ax = sns.distplot(data,
-                  kde=True,
-                  bins=100,
-                  color='skyblue',
-                  hist_kws={"linewidth": 15,'alpha':1}) 
-ax.set(xlabel='Exponential Distribution ', ylabel='Frequency')
-params = expon.fit(data)
-print("Parameters of exponential distribution",params)
-plt.show()
-plt.figure()
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.diagnostic import acorr_ljungbox
 
-# Plotting histogram of inter-arrival times towards B
-plt.hist(df['Tum'], bins=20)
-plt.title("Inter-arrival times towards B")
-plt.xlabel("Inter-arrival times")
-plt.ylabel("Frequency")
-plt.show()
+# Create your time series data as a NumPy array or a pandas Series
+# For example, assuming you have a variable 'inter_arrival_times' containing your data
 
-# As the data seem to follow Normal distribution fitting Normal distribution to the inter-arrival times towards B
+# Calculate the Ljung-Box test statistics and p-values
+lb_test_stat, lb_p_value = acorr_ljungbox(df['Ben'], lags=None)
 
-sns.set(color_codes=True)
-sns.set(rc={'figure.figsize':(5,5)})
-x = np.linspace(0, 50, 100)
-data=df['Tum']
-ax = sns.distplot(data,
-                  kde=True,
-                  bins=100,
-                  color='skyblue',
-                  hist_kws={"linewidth": 15,'alpha':1})
-ax.set(xlabel='Normal Distribution ', ylabel='Frequency')
-params = norm.fit(data)
-print("Parameters of Normal distribution",params)
+# Display the results for each lag
+for lag, test_stat, p_value in zip(range(1, len(lb_test_stat) + 1), lb_test_stat, lb_p_value):
+    print(f"Lag {lag}: Ljung-Box Test Statistic = {test_stat}, P-Value = {p_value}")
 
-plt.show()
+# You can examine each test statistic and its corresponding p-value to assess autocorrelation at different lags.
+
+
+# You can also check whether the p-values are below a significance level (e.g., 0.05) to reject the null hypothesis of no autocorrelation.
+
+
+# sns.set(color_codes=True)
+# sns.set(rc={'figure.figsize':(5,5)})
+# x = np.linspace(0, 50, 100)
+# data=df['Ben']
+# ax = sns.distplot(data,
+#                   kde=True,
+#                   bins=100,
+#                   color='skyblue',
+#                   hist_kws={"linewidth": 15,'alpha':1}) 
+# ax.set(xlabel='Exponential Distribution ', ylabel='Frequency')
+# params = expon.fit(data)
+# print("Parameters of exponential distribution",params)
+# plt.show()
+# plt.figure()
+
+# # Plotting histogram of inter-arrival times towards B
+# plt.hist(df['Tum'], bins=20)
+# plt.title("Inter-arrival times towards B")
+# plt.xlabel("Inter-arrival times")
+# plt.ylabel("Frequency")
+# plt.show()
+
+# # As the data seem to follow Normal distribution fitting Normal distribution to the inter-arrival times towards B
+
+# sns.set(color_codes=True)
+# sns.set(rc={'figure.figsize':(5,5)})
+# x = np.linspace(0, 50, 100)
+# data=df['Tum']
+# ax = sns.distplot(data,
+#                   kde=True,
+#                   bins=100,
+#                   color='skyblue',
+#                   hist_kws={"linewidth": 15,'alpha':1})
+# ax.set(xlabel='Normal Distribution ', ylabel='Frequency')
+# params = norm.fit(data)
+# print("Parameters of Normal distribution",params)
+
+# plt.show()
 
